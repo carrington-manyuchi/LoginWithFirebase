b8925ea5f244ba130bc3ce8716d79bf78984d28e
Carrington <Manyuchi>
2023-09-18 15:35:28 +0200

    Initial Commit

---
+++ LoginWithFirebase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LoginWithFirebase
+//
+//  Created by DA MAC M1 157 on 2023/09/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
